e76f2ddd4b18b366889b4d8c942cd8bf7721350f
SteveSilvas <steve.evets00@gmail.com>
2020-11-18 21:04:27 -0300

    comentando

--- src/ContandoNumeros.java
+++ src/ContandoNumeros.java
@@ -3,9 +3,9 @@ import javax.swing.JOptionPane;
 public class ContandoNumeros {
 
 	public static void main(String[] args) {
-		JOptionPane.showMessageDialog(null, "Este Programa Calcula a Quantidade de Caracteres do seu texto  (a soma inclui os espaços vazios");
+		JOptionPane.showMessageDialog(null, "Este Programa Calcula a Quantidade de Caracteres do seu texto  (a soma inclui os espaços vazios)");
 		String tec = JOptionPane.showInputDialog("digite seu texto ");
-		
+		//decare $ numCar que recebe o valor de $S tec.lenght-metodo retorna a quantidade de caracteres
 		int numCar = tec.length();
 		JOptionPane.showMessageDialog(null, "Você digitou " +numCar+ " caracteres");
 		
